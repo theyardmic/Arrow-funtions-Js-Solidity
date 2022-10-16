2c6f870481de045b310f9b53e46c2e726d7c8289
Strucker.eth <93291951+strucker-eth@users.noreply.github.com>
2022-10-16 20:28:12 +0300

    Update Js Arrows.js

--- Js Arrows.js
+++ Js Arrows.js
@@ -11,3 +11,11 @@ let welcome = (age < 18) ?
   () => console.log('Adult');
 
 welcome(); // Baby
+
+let sum = (a, b) => {
+    let result = a + b;
+    return result;
+}
+
+let result1 = sum(5,7);
+console.log(result1); // 12
